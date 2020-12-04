3e08fbf36833166306d9025ffcf8ba2224a144a0
mrgareth <mchirinoss@unsa.edu.pe>
2020-12-03 21:48:34 -0500

    Agregando modelos, falta terminar

--- fitness/models.py
+++ fitness/models.py
@@ -1,3 +1,11 @@
-from django.db import models
+from django.db import models    
 
 # Create your models here.
+class Usuario(models.Model):
+    nombre      =
+    apellido    =
+    peso(kg)    =
+    talla(cm)   =
+    edad        =
+    email       =
+
